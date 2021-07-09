019a7f055ccb0a538353b452c40ab858ca8db214
BohdanHurzhiy <g.bodyan@gmail.com>
2021-07-09 14:33:07 +0300

    First in newProject

---
+++ AppForEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppForEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
