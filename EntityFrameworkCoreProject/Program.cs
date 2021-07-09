f921655a34f4ca45045951906ec13973aff3e7b9
BohdanHurzhiy <g.bodyan@gmail.com>
2021-07-09 13:25:43 +0300

    First

---
+++ EntityFrameworkCoreProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
