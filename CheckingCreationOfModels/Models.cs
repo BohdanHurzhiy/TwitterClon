b63e6a3211aa225cbb90b1f259f98b17114c4bd2
BohdanHurzhiy <g.bodyan@gmail.com>
2021-07-13 12:57:57 +0300

    add creation Table Without navigation property and foreign key

--- CheckingCreationOfModels/Models.cs
+++ CheckingCreationOfModels/Models.cs
@@ -17,9 +17,6 @@ namespace CheckingCreationOfModels
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
-        public int CompanyId { get; set; }      // внешний ключ
-        public Company Company { get; set; }    // навигационное свойство
+        public string Name { get; set; }       
     }
 }
